1c263f550c915f4a2469c9ba1093d73e3b8c84ff
biwswalker <jennarong.sae@gmail.com>
2024-05-22 22:52:35 +0700

    remove env data in ping api

--- src/apis/v1/ping.api.ts
+++ src/apis/v1/ping.api.ts
@@ -3,6 +3,6 @@ import { Router } from 'express'
 const ping_api = Router()
 
 ping_api.get('/', async (req, res) => {
-    res.status(200).send({ api: 'ping', data: process.env })
+    res.status(200).send({ api: 'ping' })
 })
 export default ping_api
