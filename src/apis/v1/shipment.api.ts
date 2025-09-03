1b600c464a25280a3737fc77d8ba8e4c0da3825d
Biws <jennarong.sae@gmail.com>
2025-09-04 01:00:09 +0700

    remove unused code

--- src/apis/v1/shipment.api.ts
+++ src/apis/v1/shipment.api.ts
@@ -1,90 +1,24 @@
 import { Router } from 'express'
-import { shipmentNotifyQueue } from '@configs/jobQueue' // Import queue ของคุณ
-import { sendNewShipmentNotification } from '@controllers/shipmentNotification'
-import NotificationModel, {
-  ENavigationType,
-  ENotificationVarient,
-  NOTIFICATION_TITLE,
-} from '@models/notification.model'
-import { decryption } from '@utils/encryption'
+import NotificationModel, { ENotificationVarient } from '@models/notification.model'
 
 const test_api = Router()
-
-// Endpoint สำหรับทดสอบการแจ้งเตือนคนขับคนโปรด
-test_api.get('/test/favorite-driver/:shipmentId/:driverId', async (req, res) => {
-  const { shipmentId, driverId } = req.params
-  try {
-    await shipmentNotifyQueue.add({
-      shipmentId,
-      driverId,
-      stage: 'FAVORITE_DRIVER',
-      iteration: 1,
-    })
-    res.status(200).send(`Successfully added FAVORITE_DRIVER job for Shipment ID: ${shipmentId}`)
-  } catch (error) {
-    res.status(500).send(`Error: ${error.message}`)
-  }
-})
-
-// Endpoint สำหรับทดสอบการแจ้งเตือนแบบทั่วไป
-test_api.get('/test/broadcast/:shipmentId', async (req, res) => {
-  const { shipmentId } = req.params
-  try {
-    // await shipmentNotifyQueue.add({
-    //   shipmentId,
-    //   stage: 'INITIAL_BROADCAST',
-    //   iteration: 1,
-    // })
-    await sendNewShipmentNotification(shipmentId)
-    res.status(200).send(`Successfully added INITIAL_BROADCAST job for Shipment ID: ${shipmentId}`)
-  } catch (error) {
-    res.status(500).send(`Error: ${error.message}`)
-  }
-})
-
-// Endpoint สำหรับทดสอบการแจ้งเตือนแบบทั่วไป
-test_api.get('/test/push-notification/:track', async (req, res) => {
-  const { track } = req.params
-  try {
-    const _fcm = decryption('UFCM')
-    await NotificationModel.sendFCMNotification({
-      token: _fcm,
-      data: { navigation: ENavigationType.SHIPMENT, trackingNumber: track },
-      notification: { title: NOTIFICATION_TITLE, body: 'ทดสอบ Redirected!!' },
-    })
-    res.status(200).send(`Successfully added INITIAL_BROADCAST job for Shipment ID`)
-  } catch (error) {
-    res.status(500).send(`Error: ${error.message}`)
-  }
-})
 
 // Endpoint สำหรับทดสอบการแจ้งเตือนแบบทั่วไป
 test_api.get('/test/message-notification/:userId', async (req, res) => {
   const { userId } = req.params
+  const { fcmFlag } = req.query
+  console.log('fcmFlag', fcmFlag)
   try {
-    await NotificationModel.sendNotification({
-      userId,
-      varient: ENotificationVarient.INFO,
-      title: 'ทดสอบ Notification',
-      message: [`ระบบกำลังทดสอบการแจ้งเตือน`],
-    })
-    res.status(200).send(`Successfully Nottfication`)
-  } catch (error) {
-    res.status(500).send(`Error: ${error.message}`)
-  }
-})
-
-// Endpoint สำหรับทดสอบการแจ้งเตือนแบบทั่วไป
-test_api.get('/test/message-group-notification/:group', async (req, res) => {
-  const { group } = req.params
-  try {
-    if (group === 'admin') {
-      await NotificationModel.sendNotificationToAdmins({
+    await NotificationModel.sendNotification(
+      {
+        userId,
         varient: ENotificationVarient.INFO,
-        title: 'ทดสอบ Group Notification',
-        message: [`ระบบกำลังทดสอบการแจ้งเตือน Admin`],
-      })
-    }
+        title: 'ทดสอบ Notification',
+        message: [`ระบบกำลังทดสอบการแจ้งเตือน`],
+      },
+      undefined,
+      fcmFlag === 'Y',
+    )
     res.status(200).send(`Successfully Nottfication`)
   } catch (error) {
     res.status(500).send(`Error: ${error.message}`)
